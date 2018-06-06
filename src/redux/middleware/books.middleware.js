9d0effa9dd2786f4e736bddf6f55ab841fadee42
nirkaufman <nirkaufman@gmail.com>
2018-06-06 11:56:00 -0400

    respect destructuring in books middleware

--- src/redux/middleware/books.middleware.js
+++ src/redux/middleware/books.middleware.js
@@ -11,18 +11,18 @@ export const booksMiddleware = () => (next) => (action) => {
   switch (action.type) {
 
     case FETCH_BOOKS:
-      next(apiRequest({body: null, method: 'GET', url: BOOKS_URL,  BOOKS));
-      next(setLoader(true, BOOKS));
+      next(apiRequest({body: null, method: 'GET', url: BOOKS_URL, feature: BOOKS}));
+      next(setLoader({state: true, feature: BOOKS}));
       break;
 
     case `${BOOKS} ${API_SUCCESS}`:
-      next(setBooks(action.payload.items));
-      next(setLoader(false, BOOKS));
+      next(setBooks({books: action.payload.items}));
+      next(setLoader({state: false, feature: BOOKS}));
       break;
 
     case `${BOOKS} ${API_ERROR}`:
-      next(setNotification(action.payload, BOOKS));
-      next(setLoader(false, BOOKS));
+      next(setNotification({message: action.payload, feature: BOOKS}));
+      next(setLoader({state: false, feature: BOOKS}));
       break;
   }
 };
